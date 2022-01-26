fa52f366bf8d1d40ad7d9a6e6945f4161d129056
Alex Boten <alex@boten.ca>
2022-01-25 20:29:47 -0800

    chapter 6: example 2

--- chapter6/logs.py
+++ chapter6/logs.py
@@ -1,6 +1,9 @@
+from opentelemetry.sdk._logs.export import ConsoleLogExporter, BatchLogProcessor
 from opentelemetry.sdk._logs import LogEmitterProvider, set_log_emitter_provider
 from opentelemetry.sdk.resources import Resource
 
 def configure_log_emitter_provider():
     provider = LogEmitterProvider(resource=Resource.create())
     set_log_emitter_provider(provider)
+    exporter = ConsoleLogExporter()
+    provider.add_log_processor(BatchLogProcessor(exporter))
