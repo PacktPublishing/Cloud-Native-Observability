4c4e71eec53592738f3e7220bb0e199fdbfb80c1
Alex Boten <alex@boten.ca>
2022-01-26 23:14:45 -0800

    chapter 4: example 19

--- chapter4/common.py
+++ chapter4/common.py
@@ -1,8 +1,10 @@
+from flask import request
 from opentelemetry import trace
 from opentelemetry.sdk.resources import Resource
 from opentelemetry.sdk.trace import TracerProvider
 from opentelemetry.sdk.trace.export import BatchSpanProcessor, ConsoleSpanExporter
 from opentelemetry.semconv.resource import ResourceAttributes
+from opentelemetry.semconv.trace import SpanAttributes
 from local_machine_resource_detector import LocalMachineResourceDetector
 
 
@@ -22,3 +24,17 @@ def configure_tracer(name, version):
     provider.add_span_processor(span_processor)
     trace.set_tracer_provider(provider)
     return trace.get_tracer(name, version)
+
+def set_span_attributes_from_flask():
+    span = trace.get_current_span()
+    span.set_attributes(
+        {
+            SpanAttributes.HTTP_FLAVOR: request.environ.get("SERVER_PROTOCOL"),
+            SpanAttributes.HTTP_METHOD: request.method,
+            SpanAttributes.HTTP_USER_AGENT: str(request.user_agent),
+            SpanAttributes.HTTP_HOST: request.host,
+            SpanAttributes.HTTP_SCHEME: request.scheme,
+            SpanAttributes.HTTP_TARGET: request.path,
+            SpanAttributes.HTTP_CLIENT_IP: request.remote_addr,
+        }
+    )
