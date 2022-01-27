ce45a9fb4260d378aa5ce49c9fe8640f8f40bf11
Alex Boten <alex@boten.ca>
2022-01-26 20:53:12 -0800

    chapter 4: example 1

---
+++ chapter4/shopper.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+from opentelemetry import trace
+from opentelemetry.sdk.trace import TracerProvider
+from opentelemetry.sdk.trace.export import ConsoleSpanExporter, SimpleSpanProcessor
+
+def configure_tracer():
+    exporter = ConsoleSpanExporter()
+    span_processor = SimpleSpanProcessor(exporter)
+    provider = TracerProvider()
+    provider.add_span_processor(span_processor)
+    trace.set_tracer_provider(provider)
+
+if __name__ == "__main__":
+    configure_tracer()
