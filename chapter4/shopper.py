76d845141813fe9b52dcc1ce6440855096c005ce
Alex Boten <alex@boten.ca>
2022-01-26 22:53:45 -0800

    chapter 4: example 12

--- chapter4/shopper.py
+++ chapter4/shopper.py
@@ -3,16 +3,20 @@ from opentelemetry import context, trace
 from opentelemetry.sdk.resources import Resource
 from opentelemetry.sdk.trace import TracerProvider
 from opentelemetry.sdk.trace.export import BatchSpanProcessor, ConsoleSpanExporter
+from local_machine_resource_detector import LocalMachineResourceDetector
 
 
 def configure_tracer(name, version):
     exporter = ConsoleSpanExporter()
     span_processor = BatchSpanProcessor(exporter)
-    resource = Resource.create(
-        {
-            "service.name": name,
-            "service.version": version,
-        }
+    local_resource = LocalMachineResourceDetector().detect()
+    resource = local_resource.merge(
+        Resource.create(
+            {
+                "service.name": name,
+                "service.version": version,
+            }
+        )
     )
     provider = TracerProvider(resource=resource)
     provider.add_span_processor(span_processor)
