f7f3e17ce74a01e42a5948c12d8f87a9ae72dacd
Alex Boten <alex@boten.ca>
2022-01-29 20:23:17 -0800

    chapter 7: example 6

--- chapter7/common.py
+++ chapter7/common.py
@@ -43,16 +43,7 @@ def configure_meter(name, version):
 def configure_tracer(name, version):
     exporter = ConsoleSpanExporter()
     span_processor = BatchSpanProcessor(exporter)
-    local_resource = LocalMachineResourceDetector().detect()
-    resource = local_resource.merge(
-        Resource.create(
-            {
-                ResourceAttributes.SERVICE_NAME: name,
-                ResourceAttributes.SERVICE_VERSION: version,
-            }
-        )
-    )
-    provider = TracerProvider(resource=resource)
+    provider = TracerProvider()
     provider.add_span_processor(span_processor)
     trace.set_tracer_provider(provider)
     return trace.get_tracer(name, version)
