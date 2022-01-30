f1f0547baf8ca88353ef1a3a9b2843e2d52d74f9
Alex Boten <alex@boten.ca>
2022-01-29 20:13:55 -0800

    chapter 7: example 2

--- chapter7/http_request.py
+++ chapter7/http_request.py
@@ -1,5 +1,25 @@
 import requests
 
+from opentelemetry import trace
+from opentelemetry.sdk.trace import TracerProvider
+from opentelemetry.sdk.trace.export import (
+    BatchSpanProcessor,
+    ConsoleSpanExporter,
+)
+from opentelemetry.instrumentation.requests import RequestsInstrumentor
+
+
+def configure_tracer():
+    exporter = ConsoleSpanExporter()
+    span_processor = BatchSpanProcessor(exporter)
+    provider = TracerProvider()
+    provider.add_span_processor(span_processor)
+    trace.set_tracer_provider(provider)
+
+
+configure_tracer()
+RequestsInstrumentor().instrument()
+
 url = "https://www.cloudnativeobservability.com"
 resp = requests.get(url)
 print(resp.status_code)
