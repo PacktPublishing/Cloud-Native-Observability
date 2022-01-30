8e0969d30382b7ac07474293426b985c43090815
Alex Boten <alex@boten.ca>
2022-01-29 20:21:35 -0800

    chapter 7: add grocery store

---
+++ chapter7/common.py
@@ -0,0 +1,77 @@
+from flask import request
+from opentelemetry import trace
+from opentelemetry.sdk.resources import Resource
+from opentelemetry.sdk.trace import TracerProvider
+from opentelemetry.sdk.trace.export import BatchSpanProcessor, ConsoleSpanExporter
+from opentelemetry.semconv.resource import ResourceAttributes
+from opentelemetry.semconv.trace import SpanAttributes
+from local_machine_resource_detector import LocalMachineResourceDetector
+import logging
+from opentelemetry.sdk._logs.export import ConsoleLogExporter, BatchLogProcessor
+from opentelemetry.sdk._logs import (
+    LogEmitterProvider,
+    OTLPHandler,
+    set_log_emitter_provider,
+)
+
+
+def configure_logger(name, version):
+    local_resource = LocalMachineResourceDetector().detect()
+    resource = local_resource.merge(
+        Resource.create(
+            {
+                ResourceAttributes.SERVICE_NAME: name,
+                ResourceAttributes.SERVICE_VERSION: version,
+            }
+        )
+    )
+    provider = LogEmitterProvider(resource=resource)
+    set_log_emitter_provider(provider)
+    exporter = ConsoleLogExporter()
+    provider.add_log_processor(BatchLogProcessor(exporter))
+    logger = logging.getLogger(name)
+    logger.setLevel(logging.DEBUG)
+    handler = OTLPHandler()
+    logger.addHandler(handler)
+    return logger
+
+
+def configure_meter(name, version):
+    pass
+
+
+def configure_tracer(name, version):
+    exporter = ConsoleSpanExporter()
+    span_processor = BatchSpanProcessor(exporter)
+    local_resource = LocalMachineResourceDetector().detect()
+    resource = local_resource.merge(
+        Resource.create(
+            {
+                ResourceAttributes.SERVICE_NAME: name,
+                ResourceAttributes.SERVICE_VERSION: version,
+            }
+        )
+    )
+    provider = TracerProvider(resource=resource)
+    provider.add_span_processor(span_processor)
+    trace.set_tracer_provider(provider)
+    return trace.get_tracer(name, version)
+
+
+def set_span_attributes_from_flask():
+    span = trace.get_current_span()
+    span.set_attributes(
+        {
+            SpanAttributes.HTTP_FLAVOR: request.environ.get("SERVER_PROTOCOL"),
+            SpanAttributes.HTTP_METHOD: request.method,
+            SpanAttributes.HTTP_USER_AGENT: str(request.user_agent),
+            SpanAttributes.HTTP_HOST: request.host,
+            SpanAttributes.HTTP_SCHEME: request.scheme,
+            SpanAttributes.HTTP_TARGET: request.path,
+            SpanAttributes.HTTP_CLIENT_IP: request.remote_addr,
+        }
+    )
+
+
+def start_recording_memory_metrics():
+    pass
