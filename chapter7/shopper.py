84bf1c20ed03dfc70a2ba2bd0fe694f0c328fa66
Alex Boten <alex@boten.ca>
2022-01-29 20:49:41 -0800

    chapter 7: example 11

--- chapter7/shopper.py
+++ chapter7/shopper.py
@@ -1,52 +1,18 @@
 #!/usr/bin/env python3
+import logging
 import requests
-
 from opentelemetry import trace
-from opentelemetry.propagate import inject
-from opentelemetry.semconv.trace import HttpFlavorValues, SpanAttributes
-from opentelemetry.trace import Status, StatusCode
+from opentelemetry.sdk._logs import OTLPHandler
 
-from common import configure_logger, configure_meter, configure_tracer
-from local_machine_resource_detector import LocalMachineResourceDetector
-
-
-tracer = configure_tracer("shopper", "0.1.2")
-meter = configure_meter("shopper", "0.1.2")
-logger = configure_logger("shopper", "0.1.2")
+tracer = trace.get_tracer("shopper", "0.1.2")
+logger = logging.getLogger("shopper")
+logger.setLevel(logging.DEBUG)
+logger.addHandler(OTLPHandler())
 
 
 @tracer.start_as_current_span("browse")
 def browse():
-    print("visiting the grocery store")
-    with tracer.start_as_current_span(
-        "web request", kind=trace.SpanKind.CLIENT, record_exception=False
-    ) as span:
-        url = "http://localhost:5000/products"
-        span.set_attributes(
-            {
-                SpanAttributes.HTTP_METHOD: "GET",
-                SpanAttributes.HTTP_FLAVOR: HttpFlavorValues.HTTP_1_1.value,
-                SpanAttributes.HTTP_URL: url,
-                SpanAttributes.NET_PEER_IP: "127.0.0.1",
-            }
-        )
-        headers = {}
-        inject(headers)
-        span.add_event("about to send a request")
-        resp = requests.get(url, headers=headers)
-        if resp:
-            span.set_status(Status(StatusCode.OK))
-        else:
-            span.set_status(
-                Status(StatusCode.ERROR, "status code: {}".format(resp.status_code))
-            )
-        span.add_event(
-            "request sent",
-            attributes={"url": url},
-            timestamp=0,
-        )
-        span.set_attribute(SpanAttributes.HTTP_STATUS_CODE, resp.status_code)
-
+    resp = requests.get("http://localhost:5000/products")
     add_item_to_cart("orange", 5)
 
 
