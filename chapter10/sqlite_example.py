1a1eff165a39483d7b58762c495b2b72f1824479
Alex Boten <alex@boten.ca>
2022-01-30 10:23:41 -0800

    chapter 10: example 3

--- chapter10/sqlite_example.py
+++ chapter10/sqlite_example.py
@@ -1,6 +1,26 @@
 import logging
 import os
 import sqlite3
+from opentelemetry import trace
+from opentelemetry.exporter.jaeger.proto.grpc import JaegerExporter
+from opentelemetry.instrumentation.sqlite3 import SQLite3Instrumentor
+from opentelemetry.sdk.trace import TracerProvider
+from opentelemetry.sdk.resources import Resource
+from opentelemetry.sdk.trace.export import BatchSpanProcessor
+
+
+def configure_opentelemetry():
+    SQLite3Instrumentor().instrument()
+    exporter = JaegerExporter(insecure=True)
+    provider = TracerProvider(
+        resource=Resource.create({"service.name": "sqlite_example"})
+    )
+    provider.add_span_processor(BatchSpanProcessor(exporter))
+    trace.set_tracer_provider(provider)
+
+
+configure_opentelemetry()
+
 
 logging.basicConfig(level=logging.DEBUG)
 logger = logging.getLogger(__name__)
