d2c6b2ab1dd22121c5d866f84d6ada34e042571a
Alex Boten <alex@boten.ca>
2022-01-29 20:17:12 -0800

    chapter 7: example 4

--- chapter7/http_request.py
+++ chapter7/http_request.py
@@ -1,11 +1,5 @@
 import requests
 
-from opentelemetry import trace
-from opentelemetry.sdk.trace import TracerProvider
-from opentelemetry.sdk.trace.export import (
-    BatchSpanProcessor,
-    ConsoleSpanExporter,
-)
 from opentelemetry.instrumentation.requests import RequestsInstrumentor
 
 
@@ -17,21 +11,10 @@ def add_response_attributes(span, response):
     span.set_attribute("http.response.headers", str(response.headers))
 
 
-def configure_tracer():
-    exporter = ConsoleSpanExporter()
-    span_processor = BatchSpanProcessor(exporter)
-    provider = TracerProvider()
-    provider.add_span_processor(span_processor)
-    trace.set_tracer_provider(provider)
-
-
-configure_tracer()
 RequestsInstrumentor().instrument(
     name_callback=rename_span,
     span_callback=add_response_attributes,
 )
 
-
-url = "https://www.cloudnativeobservability.com"
-resp = requests.get(url)
+resp = requests.get("https://google.com")
 print(resp.status_code)
