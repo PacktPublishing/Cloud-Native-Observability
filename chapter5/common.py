87a84676208f7e76460cbc779dd375a72c2c1613
Alex Boten <alex@boten.ca>
2022-03-06 11:38:58 -0800

    chapter 5: example 17

--- chapter5/common.py
+++ chapter5/common.py
@@ -6,6 +6,34 @@ from opentelemetry.sdk.trace.export import BatchSpanProcessor, ConsoleSpanExport
 from opentelemetry.semconv.resource import ResourceAttributes
 from opentelemetry.semconv.trace import SpanAttributes
 from local_machine_resource_detector import LocalMachineResourceDetector
+from opentelemetry._metrics import get_meter_provider, set_meter_provider
+from opentelemetry.sdk._metrics import MeterProvider
+from opentelemetry.sdk._metrics.export import (
+    ConsoleMetricExporter,
+    PeriodicExportingMetricReader,
+)
+
+
+def configure_meter(name, version):
+    exporter = ConsoleMetricExporter()
+    reader = PeriodicExportingMetricReader(exporter, export_interval_millis=5000)
+    local_resource = LocalMachineResourceDetector().detect()
+    resource = local_resource.merge(
+        Resource.create(
+            {
+                ResourceAttributes.SERVICE_NAME: name,
+                ResourceAttributes.SERVICE_VERSION: version,
+            }
+        )
+    )
+    provider = MeterProvider(metric_readers=[reader], resource=resource)
+    set_meter_provider(provider)
+    schema_url = "https://opentelemetry.io/schemas/1.9.0"
+    return get_meter_provider().get_meter(
+        name=name,
+        version=version,
+        schema_url=schema_url,
+    )
 
 
 def configure_tracer(name, version):
@@ -24,6 +52,7 @@ def configure_tracer(name, version):
     provider.add_span_processor(span_processor)
     trace.set_tracer_provider(provider)
     return trace.get_tracer(name, version)
+
 
 def set_span_attributes_from_flask():
     span = trace.get_current_span()
